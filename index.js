cd8941b3f2f3f952da42eaac15a670f2d6842ea7
AkidauanaBrito <akidauana.oliveira@maisunifacisa.com.br>
2023-11-28 11:01:44 -0300

    Determinando quais caractereses podem conter na senha aleatória

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ let buttonElement = document.querySelector("#button");
 
 let sizePassword = document.querySelector("#valor");
 let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#%*"
